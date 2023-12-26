c4d5613f94bd474dee22fbbbaf5ef620303ccf5e
kgssksM1 <kgssks@gmail.com>
2023-12-26 21:31:54 +0900

    sign exp

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import First from './Pages/First';
 import Login from './Pages/Login';
-import SignUp from './Pages/SignUp';
+// import SignUp from './Pages/SignUp';
 import Main from './Pages/Main';
 import SignUpAuth from './Pages/SignUpAuth';
 import SignUpResult from './Pages/SignUpResult';
@@ -13,7 +13,6 @@ function App() {
       <Route path="/" element={<First/>}/>
       <Route path="/first" element={<First/>}/>
       <Route path="/login" element={<Login/>}/>
-      <Route path="/signup" element={<SignUp/>}/>
       <Route path="/signupAuth" element={<SignUpAuth/>}/>
       <Route path="/signupResult" element={<SignUpResult/>}/>
       <Route path="/main" element={<Main/>}/>
